cdcbfe45b2642ad4988897aa3379fc25b62e6441
bijanamirhojat <bijan_amirhojat@hotmail.com>
2021-08-28 10:39:04 +0200

    added theme correctly

--- wp-content/themes/bcreate/404.php
+++ wp-content/themes/bcreate/404.php
@@ -10,51 +10,96 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+    <main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bcreate' ); ?></h1>
-			</header><!-- .page-header -->
+        <section class="error-404 not-found">
+            <div class="window">
+                <div class="bar">
+                    <i></i>
+                    <div><b>About</b></div>
+                </div>
+                <div class="content">
+                    'Sup?
+                </div>
+            </div>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bcreate' ); ?></p>
+            </div><!-- .page-content -->
+        </section><!-- .error-404 -->
 
-					<?php
-					get_search_form();
+    </main><!-- #main -->
 
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+    <style>
+        @font-face {
+            font-family: Macintosh;
+            src: url(http://dallas.no-ip.org/assets/PrintChar21.ttf) format("truetype");
+        }
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bcreate' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
+        body {
+            font-size: 2px;
+            background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAFUlEQVQIW2NkYGD4D8SMjEACBP4DAA8oAgK5wqQTAAAAAElFTkSuQmCC);
+            background-size: 2em 2em;
+            background-position: 0 0, 1em 0, 1em -1em, 0em 1em;
+            font-family: Macintosh;
+            letter-spacing: -2px;
+            font-smoothing: none;
+        }
 
-					<?php
-					/* translators: %1$s: smiley */
-					$bcreate_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bcreate' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$bcreate_archive_content" );
+        .window {
+            width: 200em;
+            height: 160em;
+            background: white;
+            border: 1em solid black;
+            box-shadow: 1em 1em 0 0 black;
+            margin: 0 auto;
+        }
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+        .window > div {
+            padding: 2px;
+        }
 
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+        .content {
+            font-size: 16px;
+            padding: 10px 10px !important;
+        }
 
-	</main><!-- #main -->
+        .bar {
+            height: 11em;
+            text-align: center;
+            border-bottom: 1em solid black;
+            overflow: hidden;
+        }
 
+        .bar > div {
+            height: 11em;
+            width: 198em;
+            background: #ffffff;
+            background: linear-gradient(to bottom, #ffffff 0%, #ffffff 50%, #000000 51%, #000000 100%);
+            background-size: 2em 2em;
+            position: absolute;
+        }
+
+        .bar > div b {
+            height: 1.2em;
+            background: white;
+            padding: 0 0.4em;
+            line-height: 1.4em;
+            display: inline-block;
+            font-size: 9em;
+        }
+
+        .bar > i {
+            height: 7em;
+            width: 7em;
+            background: #ffffff;
+            border: 1em solid black;
+            position: absolute;
+            float: left;
+            z-index: 3;
+            margin: 1em 7em;
+            display: block;
+            box-shadow: 0 0 0 1em white;
+        }
+    </style>
 <?php
 get_footer();
+
--- wp-content/themes/bcreate/header.php
+++ wp-content/themes/bcreate/header.php
@@ -23,13 +23,11 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bcreate' ); ?></a>
-
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+			if ( is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
